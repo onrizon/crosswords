ccde0e2bdb4a2ea7ef65e861c507db1981e1a6a5
Bernard <bernardxf@gmail.com>
2026-01-06 10:36:13 -0300

    fix: level language

--- app/pages/api/level.ts
+++ app/pages/api/level.ts
@@ -1,12 +1,12 @@
+import { Locale } from '@/locales';
 import { generateTopicAndWords } from '@/services/genai';
-import { SupportedLanguage } from '@/types';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function getLevel(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const language = (req.query.language as SupportedLanguage) || 'pt';
+  const language = (req.query.language as Locale) || 'pt';
   const level = await generateTopicAndWords(language);
   res.status(200).json(level);
 }
