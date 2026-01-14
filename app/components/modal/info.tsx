02a1e69a53eb23a2058b73632b2652a29f151ae8
Bernard <bernardxf@gmail.com>
2026-01-14 11:25:57 -0300

    fix: Fundo escuro na abertura do popup de Info #5

--- app/components/modal/info.tsx
+++ app/components/modal/info.tsx
@@ -3,7 +3,6 @@ import { useTranslation } from '@/hooks/useTranslation';
 import { withData } from '@/lib/Context';
 import styles from '@/styles/Modal.module.css';
 import classNames from 'classnames';
-import { motion } from 'framer-motion';
 
 interface InfoModalProps {
   handleModal: (type: number, data: React.FC) => void;
@@ -12,55 +11,46 @@ interface InfoModalProps {
 const InfoModal: React.FC<InfoModalProps> = ({ handleModal }) => {
   const { t } = useTranslation();
   return (
-    <div className={styles.modalOverlay}>
-      <motion.div
-        initial={{ opacity: 0, scale: 0.95, y: 20 }}
-        animate={{ opacity: 1, scale: 1, y: 0 }}
-        exit={{ opacity: 0, scale: 0.95, y: 20 }}
-        className={styles.modalContent}
-      >
-        <div className={styles.modalHeader}>
-          <h2 className={styles.modalTitle}>{t('helpAndCommands')}</h2>
-        </div>
+    <>
+      <div className={styles.modalHeader}>
+        <h2 className={styles.modalTitle}>{t('helpAndCommands')}</h2>
+      </div>
 
-        <div className={styles.body}>
-          <div className={styles.section}>
-            <h3 className={styles.sectionTitle}>{t('chatCommands')}</h3>
-            <div className={styles.grid}>
-              <div className={styles.description}>
-                <span className={styles.commandNameCyan}>!refresh</span>
-                <span>{t('generateNewLevel')}</span>
-              </div>
+      <div className={styles.body}>
+        <div className={styles.section}>
+          <h3 className={styles.sectionTitle}>{t('chatCommands')}</h3>
+          <div className={styles.grid}>
+            <div className={styles.description}>
+              <span className={styles.commandNameCyan}>!refresh</span>
+              <span>{t('generateNewLevel')}</span>
+            </div>
 
-              <div className={styles.description}>
-                <span className={styles.commandNameRed}>!reset</span>
-                <span>{t('resetScore')}</span>
-              </div>
+            <div className={styles.description}>
+              <span className={styles.commandNameRed}>!reset</span>
+              <span>{t('resetScore')}</span>
+            </div>
 
-              <div className={styles.description}>
-                <span className={styles.commandNameYellow}>!pause</span>
-                <span>{t('pauseTime')}</span>
-              </div>
+            <div className={styles.description}>
+              <span className={styles.commandNameYellow}>!pause</span>
+              <span>{t('pauseTime')}</span>
+            </div>
 
-              <div className={styles.description}>
-                <span className={styles.commandNameEmerald}>
-                  !play / !resume
-                </span>
-                <span>{t('resumeTime')}</span>
-              </div>
+            <div className={styles.description}>
+              <span className={styles.commandNameEmerald}>!play / !resume</span>
+              <span>{t('resumeTime')}</span>
             </div>
           </div>
         </div>
-        <div className={styles.footer}>
-          <button
-            onClick={() => handleModal(C.CLOSED_MODAL, () => null)}
-            className={classNames(styles.btn, styles.btnPrimary)}
-          >
-            {t('ok')}
-          </button>
-        </div>
-      </motion.div>
-    </div>
+      </div>
+      <div className={styles.footer}>
+        <button
+          onClick={() => handleModal(C.CLOSED_MODAL, () => null)}
+          className={classNames(styles.btn, styles.btnPrimary)}
+        >
+          {t('ok')}
+        </button>
+      </div>
+    </>
   );
 };
 
